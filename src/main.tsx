4853093b5df822de8e40117a16b1b195a608d0d8
Shalanda Smith <shalanda512@gmail.com>
2026-04-26 11:24:48 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { hydrateRoot, createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root')!;
 
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <HelmetProvider><React.StrictMode><App /></React.StrictMode></HelmetProvider>);
-} else {
-  createRoot(rootElement).render(<HelmetProvider><React.StrictMode><App /></React.StrictMode></HelmetProvider>);
-}
+createRoot(rootElement).render(
+  <HelmetProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HelmetProvider>
+);
