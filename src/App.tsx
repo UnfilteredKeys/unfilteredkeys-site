171f50518af373277b074d3ef201a484a0e5b6e1
Shalanda Smith <shalanda512@gmail.com>
2026-05-07 22:34:47 -0500

    Add CopperasCoveVALoan route and import to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import PhysicianLoanHoustonTxPage from "@/pages/PhysicianLoanHoustonTx";
 import ConstructionRenovationTexasPage from "@/pages/ConstructionRenovationTexas";
 import PcsToPortfolioPage from "@/pages/PcsToPortfolio";
 import KilleenVaLoanPage from "@/pages/KilleenVaLoan";
+import CopperasCoveVALoanPage from "@/pages/CopperasCoveVALoan";
 import FirstTimeBuyersPage from "@/pages/FirstTimeBuyers";
 import InvestorsPage from "@/pages/Investors";
 import FhaLoanTexasPage from "@/pages/FhaLoanTexas";
@@ -68,6 +69,7 @@ const App = () => (
             <Route path="/construction-renovation-loans-texas" element={<ConstructionRenovationTexasPage />} />
             <Route path="/pcs-to-portfolio" element={<PcsToPortfolioPage />} />
             <Route path="/killeen-va-loan" element={<KilleenVaLoanPage />} />
+              <Route path="/copperas-cove-va-loan" element={<CopperasCoveVALoanPage />} />
             <Route path="/first-time-buyers" element={<FirstTimeBuyersPage />} />
             <Route path="/investors" element={<InvestorsPage />} />
             <Route path="/fha-loan-texas" element={<FhaLoanTexasPage />} />
