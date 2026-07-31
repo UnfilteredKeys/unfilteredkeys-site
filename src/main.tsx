0a29eab1c2b6099bc41a4ac43747bedd0b650a23
Shalanda Smith <shalanda512@gmail.com>
2026-07-31 09:25:23 -0500

    Initialize analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
+import { initializeAnalytics } from './lib/analytics';
 
 const rootElement = document.getElementById('root')!;
+
+initializeAnalytics();
 
 createRoot(rootElement).render(
   <HelmetProvider>
